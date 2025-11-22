706fb1852e6167a354129713877ff07c50595555
Pankaj <pankaj@example.com>
2025-11-22 18:54:35 +0530

    Fresh start

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // Replace 'your-repo-name' with the actual name of your GitHub repository
-const repoName = 'portfolio'; 
+const repoName = '/Myportfolio'; 
 
 export default defineConfig({
   plugins: [react()],
