2a6c82467e48b304824c477129e07c45a9952cef
Pankaj <pankaj@example.com>
2025-11-22 22:44:17 +0530

    make logo responsive

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Replace 'your-repo-name' with the actual name of your GitHub repository
-const repoName = '/Myportfolio'; 
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`, // CRITICAL: Sets the base URL for asset loading
-});
+  base: "./",  // ⬅️ CHANGE THIS LINE to exactly "./"
+})
